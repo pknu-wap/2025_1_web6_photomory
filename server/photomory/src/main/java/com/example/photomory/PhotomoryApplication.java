ad946e76a4f5c55de490a0886c6a4da76c295453
aranlll <163715414+aranlll@users.noreply.github.com>
2025-03-25 20:28:19 +0900

    Feat: 초기 설정
    Spring 초기 설정

---
+++ server/photomory/src/main/java/com/example/photomory/PhotomoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.photomory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotomoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotomoryApplication.class, args);
+	}
+
+}
