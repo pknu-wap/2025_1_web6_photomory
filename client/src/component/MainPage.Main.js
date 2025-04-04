8af64c6539502c381acdf778dfb3c162d0c82a63
gilnyang coding <kdw061224@gmail.com>
2025-04-04 22:19:23 +0900

    feat: 메인 페이지 초기
    모달 지울 예정

--- client/src/component/MainPage.Main.js
+++ client/src/component/MainPage.Main.js
@@ -1,18 +1,35 @@
 import styles from './MainPage.Main.module.css'
+import Modal from './Modal.Weekly.Memory';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers, faLock, faTrophy, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 
+
 function MainPageMain() {
     const nav = useNavigate();
-    const [isModalOpen, setIsModalOpen]= useState(false);
-    const onClickHandle=(event)=>{
-        nav(event.target.value);
+    const [isModalOpen, setIsModalOpen]=useState(false);
+    const [selectedImage, setSelectedImage]=useState(null);
+    
+    const openModal=(selectedImage)=>{
+        setSelectedImage(selectedImage);
+        setIsModalOpen(true);
     };
+    const closeModal=()=>{
+        setIsModalOpen(false);
+        setSelectedImage(null)
+    };
+    const onClickHandle=(event)=>{
+        nav(event.currentTarget.dataset.value);
+    };
+
+    const weeklyImage='/Image.png';
+
     return (
         <div className={styles.mainContainer}>
+            {isModalOpen && <Modal onClose={closeModal} 
+            imageSrc={selectedImage}/>}
             <div className={styles.MainLogoContainer}>
                 <div className={styles.logo}>
                     logo
@@ -23,24 +40,24 @@ function MainPageMain() {
             </div>
             <div className={styles.myMemoryContainer}
             onClick={onClickHandle}
-            value='/myMemory'>
+            data-value='/myMemory'>
                 <FontAwesomeIcon icon={faLock} className={styles.icon} /> <br></br>
                 <p className={styles.myMemoryText}>나만의 추억</p>
                 <p className={styles.myMemoryExplain}>나만 볼 수 있는 특별한 순간을 안전하게 보관하세요</p>
                 <div className={styles.myMemoryImageContainer}>
-                    <img src="" alt="" className={styles.myMemoryImage1}></img>
-                    <img src="" alt="" className={styles.myMemoryImage2}></img>
+                    <img src={weeklyImage} alt="" className={styles.myMemoryImage1}></img>
+                    <img src={weeklyImage} alt="" className={styles.myMemoryImage2}></img>
                 </div>
             </div>
             <div className={styles.ourMemoryContainer}
             onClick={onClickHandle}
-            value='/ourMemory'>
+            data-value='/ourMemory'>
                 <FontAwesomeIcon icon={faUsers} className={styles.icon} /> <br></br>
                 <p className={styles.ourMemoryText}>우리의 추억</p>
                 <p className={styles.ourMemoryExplain}>특별한 순간을 다른 사람들과 함께 나누고 소통하세요</p>
                 <div className={styles.ourMemoryImageContainer}>
-                    <img src="" alt="" className={styles.ourMemoryImage1}></img>
-                    <img src="" alt="" className={styles.ourMemoryImage2}></img>
+                    <img src={weeklyImage} alt="" className={styles.ourMemoryImage1}></img>
+                    <img src={weeklyImage} alt="" className={styles.ourMemoryImage2}></img>
                 </div>
             </div>
             <div className={styles.weeklyMemoryTitleContainer}>
@@ -54,9 +71,8 @@ function MainPageMain() {
                 </div>
             </div>
             <div className={styles.weeklyMemoryContainer1}
-            onClick={()=>setIsModalOpen(true)}
-            >{/*모달 사용 예정*/}
-                <img src="#" alt="" className={styles.weeklyMemoryImage1}></img>
+            onClick={()=>openModal(weeklyImage)}>
+                <img src={weeklyImage} alt="" className={styles.weeklyMemoryImage1}></img>
                 <div className={styles.weeklyMemoryImageText1}>
                     도시 야경 부문 1등!! by @id
                 </div>
@@ -71,8 +87,8 @@ function MainPageMain() {
                 </div>
             </div>
             <div className={styles.weeklyMemoryContainer2}
-            > {/*모달 사용 예정*/}
-                <img src="#" alt="" className={styles.weeklyMemoryImage2}></img>
+            onClick={()=>openModal(weeklyImage)} >
+                <img src={weeklyImage} alt="" className={styles.weeklyMemoryImage2}></img>
                 <div className={styles.weeklyMemoryImageText2}>
                     도시 야경 부문 2등!! by @id
                 </div>
@@ -87,8 +103,8 @@ function MainPageMain() {
                 </div>
             </div>
             <div className={styles.weeklyMemoryContainer3}
-            >{/*모달 사용 예정*/}
-                <img src="#" alt="" className={styles.weeklyMemoryImage3}></img>
+            onClick={()=>openModal(weeklyImage)}>
+                <img src={weeklyImage} alt="" className={styles.weeklyMemoryImage3}></img>
                 <div className={styles.weeklyMemoryImageText3}>
                     도시 야경 부문 3등!! by @id
                 </div>
@@ -105,7 +121,7 @@ function MainPageMain() {
             <div className={styles.morePictureContainer}>
                 <div className={styles.morePicture}
                 onClick={onClickHandle}
-                value='/'>
+                data-value='/'>
                     morePicture
                 </div>
             </div>
