a9ee10b89ee19a2b386686aa795c3c119e22245f
jinohpark371 <jinoh1030@naver.com>
2025-04-07 01:22:45 +0900

    design:앨범리스트 디자인 구현
    앨범에 사진이 4개 이하일시 비어있는 이미지 렌더링 구현

--- client/src/component/AlbumList.js
+++ client/src/component/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PaginationBar from "./PaginationBar";
+import emptyImage from "../assets/emptyImage.svg";
 
 function AlbumList({ albums }) {
   const [currentPage, setCurrentPage] = useState(1); //현재 페이지 상태
@@ -28,36 +29,80 @@ function AlbumList({ albums }) {
         <div
           key={currentAlbum.id}
           style={{
+            width: "1088px",
+            height: "297px",
             border: "1px solid #ddd",
             borderRadius: "8px",
-            padding: "16px",
+            padding: "24px",
             marginBottom: "24px",
             backgroundColor: "#fefefe",
+            boxShadow:
+              "0px 2px 4px -2px rgba(0, 0, 0, 0.1),0px 4px 6px -1px rgba(0, 0, 0, 0.1)",
           }}
         >
-          <div style={{ display: "flex" }}>
+          <div
+            style={{ display: "flex", width: "1040px", marginBottom: "23px" }}
+          >
             {/* 앨범 제목 */}
-            <h4>#{currentAlbum.title}</h4>
-            <p>총 {currentAlbum.photos.length}장의 사진들</p>
-            <p
+            <h4
               style={{
-                color: "#1a73e8",
-                textDecoration: "underline",
-                cursor: "pointer",
+                marginRight: "16px",
+                width: "180px",
+                height: "36px",
+                borderRadius: "9999px",
+                background: "#000000",
+                color: "#ffffff",
+                display: "flex", // 가로·세로 정렬을 위해 flex 사용
+                justifyContent: "center", // 가로 가운데
+                alignItems: "center", // 세로 가운데
               }}
             >
-              앨범 상세보기 (준비 중)
+              #{currentAlbum.title}
+            </h4>
+            <p
+              style={{
+                width: "112px",
+                height: "28px",
+                borderRadius: "9999px",
+                backgroundColor: "#000000",
+                color: "#ffffff",
+                textAlign: "center",
+                textDecoration: "none",
+                cursor: "pointer",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                alignSelf: "flex-end", //아래 라인 맞춤
+                fontSize: "14px",
+                marginRight: "16px",
+              }}
+            >
+              앨범 상세보기
             </p>
-            <p style={{ color: "#666", fontSize: "14px" }}>
-              생성일: {currentAlbum.createdAt}
-            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "16px", // 글 사이 간격
+                color: "#666",
+                fontSize: "14px",
+              }}
+            >
+              <p>
+                총 {currentAlbum.photos.length}장의 사진들 | 생성일:{" "}
+                {currentAlbum.createdAt}
+              </p>
+            </div>
           </div>
           {/* 사진미리보기(최대4개) */}
           <div
             style={{
+              width: "1040px",
+              height: "192px",
               display: "flex",
               flexWrap: "wrap",
-              gap: "12px",
+              gap: "16px",
               marginTop: "12px",
             }}
           >
@@ -66,7 +111,6 @@ function AlbumList({ albums }) {
               <div
                 key={photo.id}
                 style={{
-                  width: "150px",
                   textAlign: "center",
                 }}
               >
@@ -74,16 +118,34 @@ function AlbumList({ albums }) {
                   src={photo.imgUrl}
                   alt={photo.title}
                   style={{
-                    width: "100%",
-                    height: "100px",
+                    width: "248px",
+                    height: "192px",
                     objectFit: "cover",
                     borderRadius: "4px",
                     marginBottom: "4px",
                   }}
                 />
-                <div style={{ fontSize: "14px" }}>{photo.title}</div>
               </div>
             ))}
+            {/* 부족한 개수만큼 placeholder 추가 */}
+            {Array.from({ length: 4 - currentAlbum.photos.length }).map(
+              (_, idx) => (
+                <div
+                  key={`placeholder-${idx}`}
+                  style={{
+                    width: "248px",
+                    height: "192px",
+                    borderRadius: "4px",
+                    backgroundColor: "#f3f4f6",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img src={emptyImage} alt="emptyImage" />
+                </div>
+              )
+            )}
           </div>
         </div>
       ))}
