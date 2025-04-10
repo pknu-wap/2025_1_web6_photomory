48a54b3e0a94245583ed16c1f563e46791f975ac
gilnyang coding <kdw061224@gmail.com>
2025-04-10 19:40:01 +0900

    feat: 로그인 초기

--- client/src/component/LoginPage.Main.js
+++ client/src/component/LoginPage.Main.js
@@ -1,6 +1,6 @@
 import styles from './LoginPage.Main.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faLock, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function LoginPageMain() {
@@ -8,20 +8,24 @@ export default function LoginPageMain() {
     return(
         <>
             <div className={styles.loginPageMainContainer}>
-                <img className={styles.logoImage} alt="Logo"></img>
+                <img className={styles.logoImage} alt="logoImage"></img>
 
                 <div className={styles.loginContainer}>
                     <p className={styles.loginText}>Login</p>
                     <div className={styles.emailContainer}>
-                        <span className={styles.emailText}>email</span>
-                        <FontAwesomeIcon icon={faEnvelope}/>
+                        <span className={styles.emailText}>
+                            email
+                            <FontAwesomeIcon icon={faEnvelope}/>
+                        </span>
                         <input className={styles.emailInput} 
                         placeholder="이메일을 입력해줘요!"></input>
                     </div>
                     <div className={styles.pwContainer}>
-                        <span className={styles.pwText}>password</span>
-                        <FontAwesomeIcon icon={faEnvelope} 
-                        className={styles.icon}/>
+                        <span className={styles.pwText}>
+                            password
+                            <FontAwesomeIcon icon={faLock}
+                            className={styles.icon}/>
+                        </span>
                         <input className={styles.pwInput} 
                         placeholder="비밀번호를 입력해줘요!"></input>
                     </div>
