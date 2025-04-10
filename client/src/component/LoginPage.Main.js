3d7c20cc9aa811d2a0d80ba43cd691640e410c5e
gilnyang coding <kdw061224@gmail.com>
2025-04-10 22:29:36 +0900

    feat: 로그인 초기

--- client/src/component/LoginPage.Main.js
+++ client/src/component/LoginPage.Main.js
@@ -8,9 +8,12 @@ export default function LoginPageMain() {
     const [email, setEmail] = useState('')
     const [pw, setPw] = useState('')
 
-    const onChangeHandle = (className) => {
-        if (className === 'emailInput') {
-            setEmail()
+    const onChangeHandle = (e) => {
+        if (e.target.className === 'emailInput') {
+            setEmail(e.target.value)
+        }
+        if (e.target.className === 'pwInput') {
+            setPw(e.target.value)
         }
     }
 
@@ -29,7 +32,7 @@ export default function LoginPageMain() {
                         </span>
                         <input className={styles.emailInput} 
                         placeholder="이메일을 입력해줘요!"
-                        ></input>
+                        onChange={onChangeHandle}></input>
                     </div>
                     <div className={styles.pwContainer}>
                         <span className={styles.pwText}>
@@ -38,18 +41,16 @@ export default function LoginPageMain() {
                             className={styles.pwIcon}/>
                         </span>
                         <input className={styles.pwInput} 
-                        placeholder="비밀번호를 입력해줘요!"></input>
+                        placeholder="비밀번호를 입력해줘요!"
+                        onChange={onChangeHandle}></input>
                     </div>
                     <button className={styles.loginButton}>
                         <FontAwesomeIcon icon={faRightToBracket} />
                         LOGIN
-                    </button>
-                    <p className={styles.forgotPw}>비밀번호를 잊으셨나요?</p>
-                    <p className={styles.or}>또는</p>
-                    <span className={styles.notAccount}>
+                    </button> {/*기능 추가하기*/}
                         계정이 없으신가요?
-                        <sapn className={styles.signUp}>회원가입</sapn>
-                    </span>
+                        <button className={styles.signUp}>회원가입</button>
+                    
                 </div>
             </div>
         </>
