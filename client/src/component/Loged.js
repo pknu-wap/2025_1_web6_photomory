44c509bc14a584c5ebe7d336a1e016fc12375b46
gilnyang coding <kdw061224@gmail.com>
2025-04-12 02:56:43 +0900

    feat: 로그인 페이지 초기

--- client/src/component/Loged.js
+++ client/src/component/Loged.js
@@ -1,12 +1,14 @@
 import styles from './Loged.module.css'
 
 
-export default function Loged(){
+export default function Loged({name, id, pw}){
     
     return(
         <div className={styles.LogedPageContainer}>
-            <p className={styles.name}>{name}</p>
+            <p className={styles.name}>{name+','}</p>
             <p className={styles.welcome}>welcome to photomary!</p>
+            <p className={styles.LogedId}>id: {id}</p>
+            <p className={styles.LogedPw}>password: {pw}</p>
         </div>
     )
 }
--- client/src/component/LoginPage.Main.js
+++ client/src/component/LoginPage.Main.js
@@ -3,40 +3,51 @@ import GetUserLogin from '../api/GetUserLogin.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faLock, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom' 
+포커스 주기, 로그인 완료 창 마저 끝내기
 
 export default function LoginPageMain() {
-    const [email, setEmail] = useState('')
-    const [pw, setPw] = useState('')
+    const [ email, setEmail] = useState('')
+    const [ pw, setPw] = useState('')
+    const [ error, setError] = useState();
+    const [ isLoading , setIsLoading] = useState(false)
     const navigate = useNavigate();
 
-    const onChangeHandle = (e) => {
-        if (e.target.className === 'emailInput') {
-            setEmail(e.target.value)
-        }
-        if (e.target.className === 'pwInput') {
-            setPw(e.target.value)
-        }
+    const onChangeHandleEmail = (e) => {
+        setEmail(e.target.value)
+        setError('')
+    }
+    const onChangeHandlePw = (e) => {
+        setPw(e.target.value)
+        setError('')
     }
 
-    const onClickButtonLogin = () => {
-        const user = GetUserLogin.find((u) => u.email === email && u.password === pw);
-    
-        
-        if (user) {
-            navigate('/Loged', {
-            state: {
-            name: user.name
-            },
-        });
-        } 
-        else {
-            setEmail('')
-            setPw('')
+    const onClickButtonLogin = async () => {
+        setIsLoading(true);
+        try{
+            const userLogin = await GetUserLogin()
+            const user = userLogin.find((u) => u.email === email && u.password === pw);
+            if (user) { //로그인 성공
+                navigate('/Loged', {
+                state: {
+                name: user.name,
+                id: user.id,
+                pw: user.password
+                }
+            });
+            } 
+            else { //로그인 실패
+                setEmail('')
+                setPw('')
+                setError('이메일 또는 비밀번호가 잘못되었어요!')
+            }
+        } catch (error) {
+            console.error('An error occurred during login');
+            setError('로그인 중 오류가 발생했습니다.');
+        } finally {
+            setIsLoading(false); //로딩 종료
         }
     };
-
     return(
         <>
             <div className={styles.loginPageMainContainer}>
@@ -52,8 +63,9 @@ export default function LoginPageMain() {
                         </span>
                         <input className={styles.emailInput} 
                         placeholder="이메일을 입력해줘요!"
-                        onChange={onChangeHandle}
-                        value={email}></input>
+                        onChange={onChangeHandleEmail}
+                        value={email}
+                        disabled={isLoading}></input>
                     </div>
                     <div className={styles.pwContainer}>
                         <span className={styles.pwText}>
@@ -61,17 +73,21 @@ export default function LoginPageMain() {
                             <FontAwesomeIcon icon={faLock}
                             className={styles.pwIcon}/>
                         </span>
-                        <input className={styles.pwInput} 
+                        <input className={styles.pwInput}
+                        type='password'
                         placeholder="비밀번호를 입력해줘요!"
-                        onChange={onChangeHandle}
-                        value={pw}></input>
+                        onChange={onChangeHandlePw}
+                        value={pw}
+                        disabled={isLoading}></input>
                     </div>
+                    { error&& <p className={styles.error}>{error}</p>}
                     <button className={styles.loginButton}
-                    onClick={onClickButtonLogin}>
+                    onClick={onClickButtonLogin}
+                    disabled={isLoading}>
                         <FontAwesomeIcon icon={faRightToBracket} />
-                        LOGIN
+                        {isLoading? isLoading: 'LOGIN'}
                     </button>
-                        <apsn className={styles.notAccount}>계정이 없으신가요?</apsn>
+                        <span className={styles.notAccount}>계정이 없으신가요?</span>
                         <button className={styles.signUp}>회원가입</button>
                     
                 </div>
