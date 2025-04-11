499821652f78c7a0a32fc33fbb93a9c84a63187b
jinohpark371 <jinoh1030@naver.com>
2025-04-11 18:00:10 +0900

    feat:사진 댓글 기능 초기 구현

--- client/src/component/CommentBox.js
+++ client/src/component/CommentBox.js
@@ -1,47 +1,109 @@
 import React, { useState } from "react";
 
 function CommentBox({ photoId }) {
-  const [comment, setComment] = useState("");
+  const [comment, setComment] = useState(""); //입력할 댓글
+  const [comments, setComments] = useState([]); // 댓글 리스트
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (comment.trim() === "") return;
 
+    const newComment = {
+      text: comment, // 댓글
+      date: new Date(), //현재 시간 저장
+    };
+    console.log(newComment.date);
     // 댓글 서버 전송
     console.log(`📸 ${photoId}번 사진 댓글:`, comment);
-
+    setComments((prev) => [...prev, newComment]); //댓글추가
     setComment(""); // 작성 후 초기화
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ marginTop: "8px" }}>
-      <input
-        type="text"
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        placeholder="댓글을 입력하세요"
+    <div
+      style={{
+        width: "516px",
+        height: "320px",
+        backgroundColor: "#ffffff",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        padding: "12px",
+        borderRadius: "8px",
+        boxSizing: "border-box",
+      }}
+    >
+      {/* 댓글 리스트 (스크롤 영역) */}
+      <div
         style={{
-          padding: "8px",
-          width: "240px",
-          border: "1px solid #ccc",
-          borderRadius: "4px",
-          marginRight: "8px",
-        }}
-      />
-      <button
-        type="submit"
-        style={{
-          padding: "8px 12px",
-          backgroundColor: "#3b82f6",
-          color: "#fff",
-          border: "none",
-          borderRadius: "4px",
-          cursor: "pointer",
+          flex: 1,
+          overflowY: "auto",
+          marginBottom: "12px",
         }}
       >
-        작성
-      </button>
-    </form>
+        <ul style={{ paddingLeft: 0, listStyleType: "none", margin: 0 }}>
+          {comments.map((c, idx) => (
+            <li
+              key={idx}
+              style={{
+                backgroundColor: "#f3f4f6",
+                padding: "6px 10px",
+                borderRadius: "6px",
+                marginBottom: "4px",
+                fontSize: "14px",
+              }}
+            >
+              <p style={{ marginBottom: "4px" }}>사용자이름 : {c.text}</p>
+              <p style={{ fontSize: "12px", color: "#888" }}>
+                {c.date.toLocaleDateString("ko-KR")}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* 댓글 입력창 */}
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          borderWidth: "1px 0px 0px 0px",
+          borderStyle: "solid",
+          borderColor: "#E5E7EB",
+          paddingTop: "10px",
+          justifyContent: "space-between",
+        }}
+      >
+        <input
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="댓글을 입력하세요"
+          style={{
+            padding: "8px",
+            width: "400px",
+            border: "1px solid #ccc",
+            borderRadius: "4px",
+            marginRight: "8px",
+          }}
+        />
+        <button
+          type="submit"
+          style={{
+            padding: "8px 12px",
+            width: "78px",
+            backgroundColor: "#3b82f6",
+            color: "#fff",
+            border: "none",
+            borderRadius: "4px",
+            cursor: "pointer",
+          }}
+        >
+          작성
+        </button>
+      </form>
+    </div>
   );
 }
 
--- client/src/component/Photos.js
+++ client/src/component/Photos.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import CommentBox from "./CommentBox";
 import dayjs from "dayjs";
+import PaginationBar from "./PaginationBar";
 
 function Photos({ album }) {
+  const [currentPage, setCurrentPage] = useState(1); //현재 페이지 상태
+  const photosPerPage = 4; //한 페이지당  사진 갯수
+
+  // 전체 페이지 수 계산
+  const totalPages = Math.ceil(album.photos.length / photosPerPage);
+
+  // 현재 페이지의 사진진들
+  const indexOfLastPhoto = currentPage * photosPerPage; //마지막앨범
+  const indexOfFirstPhoto = indexOfLastPhoto - photosPerPage; //첫번째앨범
+  const currentPhotos = album.photos.slice(indexOfFirstPhoto, indexOfLastPhoto); //앨범 범위
+
+  // 페이지 이동 핸들러
+  const handlePageClick = (page) => {
+    setCurrentPage(page);
+  };
   return (
     <>
-      {album.photos.map((photo) => (
-        <div key={photo.id} style={{ display: "flex", gap: "24px" }}>
+      {currentPhotos.map((photo) => (
+        <div
+          key={photo.id}
+          style={{ display: "flex", gap: "24px", marginBottom: "24px" }}
+        >
           <img
             src={photo.imgUrl}
             alt={photo.title}
@@ -18,6 +38,11 @@ function Photos({ album }) {
           </div>
         </div>
       ))}
+      <PaginationBar
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageClick}
+      />
     </>
   );
 }
