99dc412dd86217094c3934da3fed40fbfb9b4e15
jinohpark371 <jinoh1030@naver.com>
2025-04-12 21:01:35 +0900

    feat:페이지네이션 버튼 그룹 4개씩 표시 및 좌우이동 기능 추가

--- client/src/component/PaginationBar.js
+++ client/src/component/PaginationBar.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PaginationBar.css";
 function PaginationBar({ currentPage, totalPages, onPageChange }) {
+  const [rangeIndex, setRangeIndex] = useState(0); //현재 보여주는 그룹 인덱스
+  const buttonsPerPage = 4; // 한 번에 보여줄 버튼 개수
+
+  const totalGroups = Math.ceil(totalPages / buttonsPerPage); // 버튼 그룹 수
+
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1); //전체 페이지 갯수 만큼 번호 배열 생성
+
+  // 현재 그룹의 시작/끝 인덱스 계산
+  const start = rangeIndex * buttonsPerPage;
+  const end = start + buttonsPerPage;
+  const visiblePages = pageNumbers.slice(start, end); // 현재 보여줄 페이지만 추출
+
+  const handlePrevGroup = () => {
+    //4개의 범위를 이동
+    if (rangeIndex > 0) {
+      setRangeIndex(rangeIndex - 1);
+    }
+  };
+
+  const handleNextGroup = () => {
+    ////4개의 범위를 복귀
+    if (rangeIndex < totalGroups - 1) {
+      setRangeIndex(rangeIndex + 1);
+    }
+  };
+
   return (
     <div style={{ textAlign: "center", marginTop: "16px" }}>
-      {pageNumbers.map((pageNum) => (
+      <button
+        onClick={handlePrevGroup}
+        disabled={rangeIndex === 0}
+        className="pagiNationNumbers"
+      >
+        ◀
+      </button>
+      {visiblePages.map((pageNum) => (
         <button
           key={pageNum}
           onClick={() => onPageChange(pageNum)}
@@ -17,6 +49,13 @@ function PaginationBar({ currentPage, totalPages, onPageChange }) {
           {pageNum}
         </button>
       ))}
+      <button
+        onClick={handleNextGroup}
+        disabled={rangeIndex === totalGroups - 1}
+        className="pagiNationNumbers"
+      >
+        ▶
+      </button>
     </div>
   );
 }
