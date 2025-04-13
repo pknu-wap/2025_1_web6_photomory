92de770d3f26f0d76e99e2372aa1e217d9633e62
jinohpark371 <jinoh1030@naver.com>
2025-04-13 14:07:51 +0900

    design:페이지 네이션 화살표 디자인 추가
    번호 이동 버튼 화살표 이미지 추가

--- client/src/component/PaginationBar.js
+++ client/src/component/PaginationBar.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "./PaginationBar.css";
+import leftButton from "../assets/leftButton.svg";
+import rightButton from "../assets/rightButton.svg";
+
 function PaginationBar({ currentPage, totalPages, onPageChange }) {
   const [rangeIndex, setRangeIndex] = useState(0); //현재 보여주는 그룹 인덱스
   const buttonsPerPage = 4; // 한 번에 보여줄 버튼 개수
 
-  const totalGroups = Math.ceil(totalPages / buttonsPerPage); // 버튼 그룹 수
+  const totalGroups = Math.ceil(totalPages / buttonsPerPage); // 버튼 그룹 수(한 그룹에 4개개)
 
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1); //전체 페이지 갯수 만큼 번호 배열 생성
 
@@ -26,7 +29,7 @@ function PaginationBar({ currentPage, totalPages, onPageChange }) {
       setRangeIndex(rangeIndex + 1);
     }
   };
-
+  console.log("rangeIndex:", rangeIndex, "totalGroups:", totalGroups);
   return (
     <div style={{ textAlign: "center", marginTop: "16px" }}>
       <button
@@ -34,7 +37,14 @@ function PaginationBar({ currentPage, totalPages, onPageChange }) {
         disabled={rangeIndex === 0}
         className="pagiNationNumbers"
       >
-        ◀
+        <img
+          src={leftButton}
+          alt="leftButton"
+          style={{
+            opacity: rangeIndex === 0 ? 0.4 : 1, //비활성화시 투명도 조절절
+            verticalAlign: "middle",
+          }}
+        />
       </button>
       {visiblePages.map((pageNum) => (
         <button
@@ -54,7 +64,14 @@ function PaginationBar({ currentPage, totalPages, onPageChange }) {
         disabled={rangeIndex === totalGroups - 1}
         className="pagiNationNumbers"
       >
-        ▶
+        <img
+          src={rightButton}
+          alt="rightButton"
+          style={{
+            opacity: rangeIndex === totalGroups - 1 ? 0.4 : 1, //비활성화시 투명도 조절
+            verticalAlign: "middle",
+          }}
+        />
       </button>
     </div>
   );
