7285819c2bce38aad1ecf8cdc0c6afc3cae75568
jinohpark371 <jinoh1030@naver.com>
2025-04-17 21:15:17 +0900

    design:그룹 초대 영역 인라인스타일 정리
    GroupEditor 컴포넌트 인라인 스타일 정리

--- client/src/component/GroupEditor.js
+++ client/src/component/GroupEditor.js
@@ -1,46 +1,25 @@
+import "./GroupEditor.css";
 import FriendSearchBar from "./FriendSearchBar";
 import AddedMemberList from "./AddedMemberList";
 import AddFriendList from "./AddFriendList";
 
 function GroupEditor({
   groupName,
-  friends, //현재 유저의 친구들
+  friends,
   addedMembers,
   setAddedMembers,
-  onFriendSearch, //친구 검색 헨들러
-  onRemoveMember, //현재 그룹 인원 삭제 헨들러
+  onFriendSearch,
+  onRemoveMember,
 }) {
   return (
-    <div
-      style={{
-        width: "1088px",
-        height: "1050px",
-        padding: "32px",
-        borderRadius: "8px",
-        background: "#ffffff",
-        margin: "0px auto",
-        boxShadow:
-          "0px 4px 6px -4px rgba(0, 0, 0, 0.1),0px 10px 15px -3px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <h2
-        style={{
-          marginBottom: "24px",
-          fontSize: "24px",
-          fontWeight: "bold",
-          lineHeight: "32px",
-        }}
-      >
-        {groupName} 그룹관리
-      </h2>
+    <div className="groupEditorContainer">
+      <h2 className="groupEditorTitle">{groupName} 그룹관리</h2>
+      {/*친구 검색 컴포넌트*/}
       <FriendSearchBar friends={friends} onSearch={onFriendSearch} />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
+      <div className="groupEditorBody">
+        {/*친구 현황 목록 및 초대 가능 컴포넌트*/}
         <AddFriendList friends={friends} />
+        {/*그룹 멤버 현황 목록 및 초대 가능 컴포넌트*/}
         <AddedMemberList
           addedMembers={addedMembers}
           onRemoveMember={onRemoveMember}
