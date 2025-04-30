a2e7052d5ec08412b9e6908a87b05f7046391c55
JunYoung <pde4735@naver.com>
2025-05-01 00:05:51 +0900

    로그인 토큰 + 유저정보
    .

--- server/photomory/src/main/java/com/example/photomory/dto/TokenResponseDto.java
+++ server/photomory/src/main/java/com/example/photomory/dto/TokenResponseDto.java
@@ -5,10 +5,14 @@ public class TokenResponseDto {
     private String accessToken;
     private String refreshToken;
     private String tokenType = "Bearer"; // 일반적으로 Bearer 토큰 방식을 사용
+    private String userName;
+    private String userEmail;
 
-    public TokenResponseDto(String accessToken, String refreshToken) {
+    public TokenResponseDto(String accessToken, String refreshToken, String userName, String userEmail) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.userName = userName;
+        this.userEmail = userEmail;
     }
 
     public String getAccessToken() {
@@ -33,5 +37,21 @@ public class TokenResponseDto {
 
     public void setTokenType(String tokenType) {
         this.tokenType = tokenType;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 }
