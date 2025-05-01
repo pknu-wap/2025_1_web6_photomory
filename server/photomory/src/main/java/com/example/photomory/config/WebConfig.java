a2fefcdf8b62ac1262543dfcdd6fa9a4433452da
aranlll <163715414+aranlll@users.noreply.github.com>
2025-05-01 20:19:51 +0900

    Fix: 다시 수정

--- server/photomory/src/main/java/com/example/photomory/config/WebConfig.java
+++ server/photomory/src/main/java/com/example/photomory/config/WebConfig.java
@@ -10,7 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // 프론트엔드 주소
-                .allowedOrigins("https://photomory.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
