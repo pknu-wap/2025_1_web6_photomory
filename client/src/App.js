c5773512ae6a88a1b4fecc65da6c0d71bdde004b
jinohpark371 <jinoh1030@naver.com>
2025-05-17 15:39:23 +0900

    fix:mainpage 파일 경로 불일치 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthProvider from "./contexts/AuthContext";
-import MainPage from "./page/mainPage.js";
+import MainPage from "./page/MainPage.js";
 import LoginPage from "./page/LoginPage.js";
 import Loged from "./component/login/Loged.js";
 import ProfilePage from "./page/ProfilePage.js";
