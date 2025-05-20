35f8e513bca8b0d29f37f40460daea2361c0ae6a
JunYoung <pde4735@naver.com>
2025-05-20 14:21:11 +0900

    Fix: Album, Post 매핑을 위한 수정
    .

--- server/photomory/src/main/java/com/example/photomory/repository/PostRepository.java
+++ server/photomory/src/main/java/com/example/photomory/repository/PostRepository.java
@@ -2,6 +2,13 @@ package com.example.photomory.repository;
 
 import com.example.photomory.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    // 특정 앨범에 속한 모든 포스트 조회용 메서드 추가
+    List<Post> findByAlbum_AlbumId(Integer albumId);
 }
