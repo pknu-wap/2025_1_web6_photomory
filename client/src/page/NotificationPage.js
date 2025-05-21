dbdc050220ee6309590abcc207ecd6707ea88266
jinohpark371 <jinoh1030@naver.com>
2025-05-21 19:25:05 +0900

    feat:알림 페이지 초기 페이지 생성

---
+++ client/src/page/NotificationPage.js
@@ -0,0 +1,10 @@
+import Footer from "../component/common/Footer";
+import Header from "../component/common/Header";
+export default function NotificationPage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
