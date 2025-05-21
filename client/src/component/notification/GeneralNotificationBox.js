deae7791e1d1d56dd682466894bc1f06d2573ec9
jinohpark371 <jinoh1030@naver.com>
2025-05-21 20:27:57 +0900

    feat:알림 페이지 라우팅 경로 생성

---
+++ client/src/component/notification/GeneralNotificationBox.js
@@ -0,0 +1,11 @@
+import GeneralNotificationItem from "./GeneralNotificationItem";
+
+function GeneralNotificationBox() {
+  return (
+    <div>
+      <GeneralNotificationItem />
+    </div>
+  );
+}
+
+export default GeneralNotificationBox;
--- client/src/page/NotificationPage.js
+++ client/src/page/NotificationPage.js
@@ -1,10 +1,25 @@
 import Footer from "../component/common/Footer";
 import Header from "../component/common/Header";
-export default function NotificationPage() {
+import MemoryNotificationBox from "../component/notification/MemoryNotificationBox";
+import GeneralNotificationBox from "../component/notification/GeneralNotificationBox";
+function NotificationPage() {
   return (
     <>
       <Header />
+      <div
+        style={{
+          width: "1056px",
+          height: "1068px",
+          borderRadius: "8px",
+          background: "#ffffff",
+        }}
+      >
+        <MemoryNotificationBox />
+        <GeneralNotificationBox />
+      </div>
       <Footer />
     </>
   );
 }
+
+export default NotificationPage;
