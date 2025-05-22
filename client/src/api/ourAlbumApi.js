f5f3d1e76685e2c835916ea0fb11a45da2827cc3
jinohpark371 <jinoh1030@naver.com>
2025-05-22 16:02:21 +0900

    feat:우리의 추억 앨버 생성 api 연동 초기 구현

--- client/src/api/ourAlbumApi.js
+++ client/src/api/ourAlbumApi.js
@@ -1,6 +1,6 @@
 const BASE_URL = process.env.REACT_APP_API_URL;
 
-//그룹 생성 api함수
+//우리의 추억 그룹 생성 api함수
 export async function createGroup({ groupName, groupDescription }) {
   const token = localStorage.getItem("accessToken"); // 인증이 필요하다면 사용
 
@@ -29,7 +29,7 @@ export async function createGroup({ groupName, groupDescription }) {
   }
 }
 
-//그룹 정보 불러오기 api함수
+//우리의 추억 그룹 정보 불러오기 api함수
 export async function fetchGroupInfo(groupId) {
   const token = localStorage.getItem("accessToken"); // 필요시 사용
 
@@ -50,6 +50,43 @@ export async function fetchGroupInfo(groupId) {
     return data; // 그룹 정보 + members 배열
   } catch (error) {
     console.error("그룹 조회 중 에러:", error);
+    throw error;
+  }
+}
+
+//우리의 추억 앨범 생성 api 함수
+export async function createGroupAlbum(
+  groupId,
+  { albumName, albumTag, albumMakingTime, albumDescription }
+) {
+  const token = localStorage.getItem("accessToken");
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}/api/our-album/group/${groupId}/album`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          albumName,
+          albumTag,
+          albumMakingTime,
+          albumDescription,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("앨범 생성 실패: " + response.status);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("앨범 생성 중 오류 발생:", error);
     throw error;
   }
 }
