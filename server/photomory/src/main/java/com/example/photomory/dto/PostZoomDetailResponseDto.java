bff5b4783531125d17b3e2e36fb2a60ae4fa03df
JunYoung <pde4735@naver.com>
2025-05-23 16:30:58 +0900

    Revert "Fix: 게시물상쇄보기에서 좋아요 제거"
    This reverts commit 3de9ecfd482f1d65639a82a5e416d57278417953.

--- server/photomory/src/main/java/com/example/photomory/dto/PostZoomDetailResponseDto.java
+++ server/photomory/src/main/java/com/example/photomory/dto/PostZoomDetailResponseDto.java
@@ -16,7 +16,7 @@ public class PostZoomDetailResponseDto {
     private String postText;
     private String postDescription;
     private String photoUrl;
-    // private Integer likesCount; // 좋아요 수 필드 제거
+    private Integer likesCount;
     private List<CommentResponseDto> comments;
 
     public static PostZoomDetailResponseDto from(Post post, List<Comment> comments) {
@@ -25,7 +25,7 @@ public class PostZoomDetailResponseDto {
         dto.setPostText(post.getPostText());
         dto.setPostDescription(post.getPostDescription());
         dto.setPhotoUrl(post.getPhotoUrl());
-        // dto.setLikesCount(post.getLikesCount()); // 좋아요 수 설정 라인 제거
+        dto.setLikesCount(post.getLikesCount());
         dto.setComments(comments.stream()
                 .map(CommentResponseDto::fromEntity)
                 .collect(Collectors.toList()));
