22949d6ea7bae0583952a3fa10d2e0e4be2e4c19
gilnyang coding <kdw061224@gmail.com>
2025-05-25 19:13:55 +0900

    style: 컨테이너 부분 삭제

--- client/src/page/ProfilePage.js
+++ client/src/page/ProfilePage.js
@@ -1,15 +1,14 @@
 import Header from "../component/common/Header";
 import ProfileMain from "../component/profile/Profile.Main.js";
 import Footer from "../component/common/Footer";
-import styles from "./Profile.Page.module.css";
 
 function ProfilePage() {
   return (
-    <div className={styles.pageContainer}>
+    <>
       <Header />
       <ProfileMain />
-      <Footer className={styles.footer} />
-    </div>
+      <Footer/>
+    </>
   );
 }
 
