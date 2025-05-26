99e304ec07335204dba6dc1dbe2945e5103a45e7
JunYoung <pde4735@naver.com>
2025-05-26 10:42:19 +0900

    Revert "Fix: 사진생성할때 태그 문자열 -> 배열"
    This reverts commit c7c73efa6f136fa049f0ef90bea28ca6db7b688d.

--- server/photomory/src/main/java/com/example/photomory/dto/AlbumCreateRequestDto.java
+++ server/photomory/src/main/java/com/example/photomory/dto/AlbumCreateRequestDto.java
@@ -3,13 +3,12 @@ package com.example.photomory.dto;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
 public class AlbumCreateRequestDto {
     private String albumName;
-    private List<String> albumTags;  // 여기서 배열(리스트)로 받음
+    private String albumTag;
     private LocalDateTime albumMakingTime;
     private String albumDescription;
 }
