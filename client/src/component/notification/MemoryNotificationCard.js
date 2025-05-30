e01b173be7e3f40a2fa9150682680e71ce1b2f8c
jinohpark371 <jinoh1030@naver.com>
2025-05-31 01:57:45 +0900

    style:MemoryNotificationCard 관련 CSS 클래스명 camelCase로 수정

--- client/src/component/notification/MemoryNotificationCard.js
+++ client/src/component/notification/MemoryNotificationCard.js
@@ -4,21 +4,21 @@ import clock from "../../assets/clock.svg";
 function MemoryNotificationCard({ data }) {
   const { message, photoUrl, title, date } = data;
   return (
-    <div className="memory-card">
+    <div className="memoryCard">
       <div className="memoryTime">
         <img src={clock} alt="clock" />
-        <span className="memory-message">{message}</span>
+        <span className="memoryMessage">{message}</span>
       </div>
-      <div className="memory-content">
+      <div className="memoryContent">
         {photoUrl ? (
-          <img src={photoUrl} alt={title} className="memory-image" />
+          <img src={photoUrl} alt={title} className="memoryImage" />
         ) : (
-          <div className="memory-image-placeholder">Image</div>
+          <div className="memoryImagePlaceholder">Image</div>
         )}
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <p className="memory-title">{title}</p>
-          <p className="memory-date">{date}</p>
-          <button className="memory-button">다시보기</button>
+          <p className="memoryTitle">{title}</p>
+          <p className="memoryDate">{date}</p>
+          <button className="memoryButton">다시보기</button>
         </div>
       </div>
     </div>
