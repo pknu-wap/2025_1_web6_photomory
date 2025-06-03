ecb96391cf4fa1c9ddcbd3545482c6c4bc328b54
gilnyang coding <kdw061224@gmail.com>
2025-06-03 12:36:52 +0900

    fix: 목이미지 삭제

--- client/src/component/ourMemory/DailyPopularTagModal.js
+++ client/src/component/ourMemory/DailyPopularTagModal.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './DailyPopularTagModal.module.css';
-import mokImage from '../../assets/mainPageImage2.svg'
+
 
 const DailyPopularTagModal = ({ isOpen, onClose, imageForModal, post}) => { //post 추가해야 함 지금은 안 써서 안 씀
   if (!isOpen) return null;
 
   return (
-    <div className={styles.modalOverlay} onClick={onClose}> {/*{imageeFormodal? imageFormodal : post}*/}
-        <img className={styles.image} src={mokImage} alt=''
+    <div className={styles.modalOverlay} onClick={onClose}> 
+        <img className={styles.image} src={imageForModal? imageForModal : post} alt=''
         onClick={e => e.stopPropagation()}></img>
     </div>
   )
